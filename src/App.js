998dc80c98b091392bfb40cc592e46c71c51eddc
Kenji Hongo <chromatic.kenji@gmail.com>
2024-04-06 02:31:31 -0700

    Tweet works!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
 import { Tweet } from 'react-twitter-widgets';
 
-
-function App() {
-  const [queue, setQueue] = useState([]);
-  const [input, setInput] = useState('');
-
-  const TweetButton = () => {
-    const tweetText = "Check out this awesome tweet!"; // Set your tweet text here
-    
-    return (
-      <div>
-        <button onClick={() => window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`)}>
-          Tweet
-        </button>
-      </div>
-    );
-  };
+const TweetButton = () => {
+  const tweetText = "Check out this awesome tweet!"; // Set your tweet text here
   
-  export default TweetButton;
+  return (
+    <div>
+      <button onClick={() => window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`)}>
+        Tweet
+      </button>
+    </div>
+  );
+};
+
+export default TweetButton;
+
